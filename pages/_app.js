19de7aa7a5ff1face935407f7f17a04dcc82be1c
KKharidev <kkharidev@gmail.com>
2022-10-06 21:01:37 +0530

    Layout change

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,20 @@
 import '../styles/globals.css'
 import { Layout } from '../components'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 
-const MyApp=({ Component, pageProps })=> {
-  const [domLoaded, setDomLoaded] = useState(false);
-  useEffect(() => {
-    setDomLoaded(true);
-  }, []);
-    return <>
-      <Head>
-        <link
-          rel="preload"
-          href="../public/fonts/Elianto-Regular.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <title>K K HARIDEV</title>
-        <meta name="description" content="Portfolio site" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        {domLoaded && (<Component {...pageProps} />)}
-      </Layout>
-    </>
+const MyApp = ({ Component, pageProps }) => {
+
+  return <>
+    <Head>
+      <title>K K HARIDEV</title>
+      <meta name="description" content="Portfolio site" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
 }
 
 export default MyApp
