fa971a34728eb309a38612e648ee8c56417b8db7
KKharidev <kkharidev@gmail.com>
2022-10-06 19:20:40 +0530

    Updated config file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  basePath: '/ kkharidev_personal',
 }
 
 module.exports = nextConfig
