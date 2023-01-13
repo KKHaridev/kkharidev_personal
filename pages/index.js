9090df7fa42170037ef5fcb5d65445ab66c9f399
KKharidev <kkharidev@gmail.com>
2023-01-13 16:36:46 +0530

    Updated landing page

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,42 @@ import Head from 'next/head'
 import Image from 'next/image'
 import style from "../styles/Hero.module.css";
 import { Container1 } from '../components'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 
-
-export default function Home() {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://api-ap-south-1.hygraph.com/v2/cl9b3c6ik3i1o01ug0c8l5cbv/master',
+    cache: new InMemoryCache()
+  });
+  const { data } = await client.query({
+    query: gql`
+    query Homes {
+  homes {
+    createdAt
+    title
+    designation
+    description
+  }
+}
+  `
+  });
+  return {
+    props: {
+      homeData: data.homes
+    },
+    revalidate: 10,
+  }
+}
+export default function Home({homeData}) {
   return (<>
     
 
     <Container1>
       <div className={style.hero}>
-        <h1 className={style.h1}>K K HARIDEV</h1>
-        <h2 className={style.h2}>Front End Developer</h2>
-        <p className={style.p}>A young enthusiast pursuing B.Tech in Computer Science and Engineering.</p>
+        <h1 className={style.h1}>{homeData[0].title}</h1>
+        <h2 className={style.h2}>{homeData[0].designation}</h2>
+        <p className={style.p}>{homeData[0].description}</p>
       </div>
     </Container1 >
 
