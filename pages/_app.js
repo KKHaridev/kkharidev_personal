6563ccc8d948fcd4c2c102c7b2ce03c26cd8cdc5
K K Haridev <67409998+KKHaridev@users.noreply.github.com>
2023-08-11 23:39:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,12 @@ const MyApp = ({ Component, pageProps }) => {
       <title>K K HARIDEV</title>
       <meta name="description" content="Portfolio site" />
       <link rel="icon" href="/favicon.ico" />
+    <script
+			defer
+			src="https://umami.algata.in/script.js"
+			data-website-id="980d1d02-7ad4-407a-9fa0-a647a504dfd7"
+			data-domains="kkharidev.algata.in"
+		></script>
     </Head>
     <Layout>
       <Component {...pageProps} />
